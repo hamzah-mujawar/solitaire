e523ce58cfe52ce865e3b6a815090f300b3cab7e
hamzah <hamzah1faisal@gmail.com>
2024-11-11 23:42:48 +0000

    added tests folder, added dummy test to ensure Gtest library is working

--- src/windows.c
+++ src/windows.c
@@ -1,4 +1,14 @@
 #include "../include/windows.h"
+#include "../include/common.h"
+#include <ncurses.h>
+#include <errno.h>
+#include <stdlib.h>
 
+void win_malloc(WIN **win){
+    if(!(*win = malloc(sizeof(**win)))){
+        error_generic(errno, __FILE__, __LINE__);
+    }
+    (*win)->win = newwin(WIN_HEIGHT, WIN_WIDTH, 0, 0);
+}
 
 
